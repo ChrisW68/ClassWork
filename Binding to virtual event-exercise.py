b02e422f1000bee4017a7adaa116e9a397f10c99
Chris W <chris.wiseman74@gmail.com>
2016-05-17 10:39:06 -0700

    Add files via upload

---
+++ Binding to virtual event-exercise.py
@@ -0,0 +1,26 @@
+#python3
+
+# This is a class assignment in lynda.com Python GUI Development with Tkinter
+
+from tkinter import *
+from tkinter import ttk        
+    
+root = Tk()
+
+entry = ttk.Entry(root)
+entry.pack()
+
+entry.bind('<<Copy>>', lambda e: print('Copy'))
+entry.bind('<<Paste>>', lambda e: print('Paste'))
+
+entry.event_add('<<OddNumber>>','1', '3', '5', '7', '9')
+entry.bind('<<OddNumber>>', lambda e: print('Odd Number'))
+
+print(entry.event_info('<<OddNumber>>'))
+
+
+entry.event_generate('<<OddNumber>>')
+entry.event_generate('<<Paste>>')
+
+entry.event_delete('<<OddNumber>>')
+root.mainloop()
