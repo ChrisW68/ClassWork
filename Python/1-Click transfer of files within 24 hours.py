265dc1b9d8f06a09134ca0921160fe9aedc9d97a
Chris W <chris.wiseman74@gmail.com>
2016-06-05 21:05:52 -0700

    Added more detail
    Add more detail on exactly what the program is doing throughout the process

--- Python/1-Click transfer of files within 24 hours.py
+++ Python/1-Click transfer of files within 24 hours.py
@@ -9,11 +9,12 @@ wildcard = "All files (*.txt)|*.*"
 class MyForm(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, wx.ID_ANY,
-                          "File and Folder Dialogs Tutorial")
+                          "File and Folder Dialogs Transfer")
         panel = wx.Panel(self, wx.ID_ANY)
         self.currentDirectory = os.getcwd()
- 
-        actBtn = wx.Button(panel, label="Transfer the file")
+        
+        #Action button to transfer files from src(source folder) to dst(destination folder)
+        actBtn = wx.Button(panel, label="Transfer the file") 
         actBtn.Bind(wx.EVT_BUTTON, self.onDir)
 
         #put the buttons in a sizer
@@ -21,7 +22,7 @@ class MyForm(wx.Frame):
         sizer.Add(actBtn, 0, wx.ALL|wx.CENTER,5)
         panel.SetSizer(sizer)
         
-
+    # Source and Destination of files.
     def onDir(src,dst):
         src = 'C:\\Users\\wise_\\Desktop\\Folder A'
         dst = 'C:\\Users\\wise_\\Desktop\\Folder B'
@@ -31,12 +32,17 @@ class MyForm(wx.Frame):
             src_file = os.path.join(src, file)
             dst_file = os.path.join(dst, file)
             st=os.stat(src_file)
+            #Source files age in seconds
             ctime=st.st_ctime
+            #Converts time from seconds to hours
             mtime=(time.time() - ctime)/3600
+            #If creation time is less than 24 hours then move files from source location to destination
             if mtime<24:
                 shutil.move(src_file, dst_file)
+                #Print all files that have been moved to destination location
                 print dst_file
             else:
+                #This will print the age of the files in hours
                 print "file are: '%d' hours, since creation/modification" %mtime
 
 
