fea2f47bdcb87a08bf4b5f6d4e4bca8822da99cd
Chris <chris.wiseman74@gmail.com>
2016-07-12 22:23:34 -0700

    Setting and retrieve values from a cookie

--- PHP/mysql/cookies.php
+++ PHP/mysql/cookies.php
@@ -4,7 +4,7 @@ $name = "Chris";
 $value = 100;
 $expiration = time() + (60*60*24*5);  //adding 5 days to current time
 
-setcookie('$name', '$value', '$expiration');
+setcookie($name,$value,$expiration);  //This will set the cookie automatically when this program runs
 
 ?>
 <!DOCTYPE html>
@@ -15,7 +15,17 @@ setcookie('$name', '$value', '$expiration');
 </head>
 <body>
    
-   
+<?php   
+//The below function will check the value of the cookie for $name and echo the value of the cookie
+if(isset($_COOKIE['Chris'])) {  
+    $someOne = $_COOKIE["Chris"];
     
+    echo "The value of cookie " . $someOne . " was set";
+} else {
+    $someOne = "";
+}
+    
+    
+?>    
 </body>
 </html>
