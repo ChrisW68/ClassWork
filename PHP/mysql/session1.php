3781244a57ee2af37570a3a0ce9eb0cd03e505f7
Chris <chris.wiseman74@gmail.com>
2016-07-12 22:49:41 -0700

    Created sessions to show information stored in cookies

---
+++ PHP/mysql/session1.php
@@ -0,0 +1,18 @@
+<?php session_start();
+
+$_SESSION['greeting'] = "Thanks for coming";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+   
+    
+</body>
+</html>
